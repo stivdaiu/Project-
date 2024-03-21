73c084c1e9220c3c9270133c336ff4cd3d4c668e
stivdaiu <122056868+stivdaiu@users.noreply.github.com>
2024-03-21 01:51:17 +0100

    Update InvoiceField.jsx

--- src/components/InvoiceField.jsx
+++ src/components/InvoiceField.jsx
@@ -1,21 +1,41 @@
 import React from 'react';
 
 const InvoiceField = ({ onEditItem, cellData }) => {
-  return (
-    <input
-      className={cellData.className}
-      type={cellData.type}
-      placeholder={cellData.placeholder}
-      min={cellData.min}
-      max={cellData.max}
-      step={cellData.step}
-      name={cellData.name}
-      id={cellData.id}
-      value={cellData.value}
-      onChange={onEditItem}
-      required
-    />
-  );
+  const handleChange = (event) => {
+    onEditItem(event);
+  };
+
+  if (cellData.type === 'number') {
+    return (
+      <input
+        className={cellData.className}
+        type="number"
+        min={cellData.min}
+        max={cellData.max}
+        step={cellData.step}
+        name={cellData.name}
+        id={cellData.id}
+        value={cellData.value}
+        onChange={handleChange}
+        required
+      />
+    );
+  } else if (cellData.type === 'text') {
+    return (
+      <input
+        className={cellData.className}
+        type="text"
+        placeholder={cellData.placeholder}
+        name={cellData.name}
+        id={cellData.id}
+        value={cellData.value}
+        onChange={handleChange}
+        required
+      />
+    );
+  } else {
+    return null;
+  }
 };
 
 export default InvoiceField;
